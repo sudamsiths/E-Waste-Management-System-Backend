08cf810e741ec3938522f159852ab62a8b9cc7ab
sudamsiths <sudamsiths.com>
2025-07-16 15:41:34 +0530

    Initial commit for Recycling Center Microservice

---
+++ src/main/java/com/icet/project/repository/RecyclingCenterRepository.java
@@ -0,0 +1,7 @@
+package com.icet.project.repository;
+
+import com.icet.project.model.entity.Recycling_CenterEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecyclingCenterRepository extends JpaRepository<Recycling_CenterEntity , Long> {
+}
