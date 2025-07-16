9dbc64b04ac416f3ac40d64b49e8664aeede6f38
sudamsiths <sudamsiths.com>
2025-07-16 15:44:29 +0530

    Add micro service

---
+++ src/main/java/com/icet/project/model/entity/FeedbackEntity.java
@@ -0,0 +1,30 @@
+package com.icet.project.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name="feedback")
+public class FeedbackEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private Long feedbackId;
+    private String customerName;
+    private String email;
+    private Date submittedAt;
+    private String message;
+
+//    @ManyToOne
+//    @JoinColumn(name = "user_id")
+//    private UsersEntity user;
+
+}
